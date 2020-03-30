22bdbd5dc3504ee56127f66ee52f29fd9caff041
JerlyM05 <jerlymigueldelarosah@gmail.com>
2020-03-30 13:47:37 -0400

    primer commit
     comiencen con '#' serán ignoradas, y un mensaje

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { Button } from 'reactstrap'
+
+
+class Title extends Component{
+    render() {
+      return <h2>{this.props.nombre}</h2>
+    }
+}
+
+Title.defaultProps = { 
+  nombre: 'Titulo Por Defecto'
+}
+
+
+
+class App extends Component {
+  render() {
+    
+    return (  
+          <div className="App">
+              
+             <Title nombre='Nuevo titulo'/>
+             <Title nombre='Nuevo titulo2'/>
+             <Title nombre='Nuevo titulo3'/>
+             <Button color="danger">Danger!</Button>
+      </div>
+      
+    );          
+  }
+}
+
+export default App;
